6bf108e180758b1d68a16b351d35ed6efd0ec6ba
she <2185902575@qq.com>
2021-09-12 21:16:29 +0800

    django项目第一次修改

---
+++ user/tests.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+
+# # Create your tests here.
+# for i in range(20):
+#     if i == 9:
+#         break
+#     print(i)
+#     else:
+#         print("循环结束")
+#
+# import re
+#
+# a = "I am a smart boy"
+# num = 's\nmss'
+# print(num.strip())
+# b = re.sub(r'\s', '', a)
+# print(b)
+# list1 = [i for i in b]
+# print(list1)
+# list2 = [i for i in a.split()]
+# print(list2)
+# li = [1, 2, 3]
+# num = li.sort()
+# print(li)
+
+class t(object):
+    a = 1
+
+    def t(self):
+        hum = 90
+te = t()
+print(te.t())
